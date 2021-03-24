6b64722f4600d360910cd2254c2c579cf20e6bc7
MJendza <mjendza@newforma.com>
2021-03-24 23:41:57 +0100

    alpha version for storage

---
+++ assets/lambda/src/users/userRepository.ts
@@ -0,0 +1,29 @@
+import { DocumentClient } from 'aws-sdk/lib/dynamodb/document_client';
+import { UserEntity } from './userEntity';
+
+const TABLE_NAME = process.env.TABLE_NAME || '';
+
+const RESERVED_RESPONSE = `Error: You're using AWS reserved keywords as attributes`,
+    DYNAMODB_EXECUTION_ERROR = `Error: Execution update, caused a Dynamodb error, please take a look at your CloudWatch Logs.`;
+export class UserRepository {
+    private documentClient: DocumentClient;
+    constructor() {
+        this.documentClient = new DocumentClient();
+    }
+    async put(item: UserEntity): Promise<void> {
+        const params = {
+            TableName: TABLE_NAME,
+            Item: item,
+        };
+        try {
+            await this.documentClient.put(params).promise();
+        } catch (dbError) {
+            const errorResponse =
+                dbError.code === 'ValidationException' &&
+                dbError.message.includes('reserved keyword')
+                    ? DYNAMODB_EXECUTION_ERROR
+                    : RESERVED_RESPONSE;
+            throw new Error(`DynamoDB ERROR: ${errorResponse}`);
+        }
+    }
+}
--- cdk/dynamoDBStack.ts
+++ cdk/dynamoDBStack.ts
@@ -1,24 +1,30 @@
-import * as dynamodb from '@aws-cdk/aws-dynamodb';
-import * as cdk from '@aws-cdk/core';
+import { Table, AttributeType, ProjectionType } from '@aws-cdk/aws-dynamodb';
+import { App, RemovalPolicy, Stack } from '@aws-cdk/core';
 
-export class DynamoDBStack extends cdk.Stack {
-    constructor(app: cdk.App, id: string) {
+export class DynamoDBStack extends Stack {
+    constructor(app: App, id: string) {
         super(app, id);
 
-        new dynamodb.Table(this, 'items', {
+        const users = new Table(this, 'users', {
             partitionKey: {
-                name: 'itemId',
-                type: dynamodb.AttributeType.STRING,
+                name: 'id',
+                type: AttributeType.STRING,
             },
-            tableName: 'items',
+            tableName: 'users',
 
             // The default removal policy is RETAIN, which means that cdk destroy will not attempt to delete
             // the new table, and it will remain in your account until manually deleted. By setting the policy to
             // DESTROY, cdk destroy will delete the table (even if it has data in it)
-            removalPolicy: cdk.RemovalPolicy.DESTROY, // NOT recommended for production code
+            removalPolicy: RemovalPolicy.DESTROY, // NOT recommended for production code
+        });
+
+        users.addGlobalSecondaryIndex({
+            indexName: 'email',
+            partitionKey: { name: 'email', type: AttributeType.STRING },
+            projectionType: ProjectionType.ALL,
         });
     }
 }
-const app = new cdk.App();
+const app = new App();
 new DynamoDBStack(app, 'DynamoDBExample');
 app.synth();
