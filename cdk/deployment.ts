aef3ab23aabd3dc65c3004a7c208c74078d7fe3d
MJendza <mjendza@newforma.com>
2021-06-22 22:19:06 +0200

    dynamo resolver

--- cdk/deployment.ts
+++ cdk/deployment.ts
@@ -57,7 +57,13 @@ import {
     useEventBridgeLambdaHandler,
 } from './helpers/event-bridge/lambda-helpers';
 import { StartingPosition } from '@aws-cdk/aws-lambda';
-import { AuthorizationType, GraphqlApi, Schema } from '@aws-cdk/aws-appsync';
+import {
+    AuthorizationType,
+    DynamoDbDataSource,
+    GraphqlApi,
+    MappingTemplate,
+    Schema,
+} from '@aws-cdk/aws-appsync';
 
 export class Deployment extends Stack {
     private lambdaSourceCode = 'assets/lambda/dist/handlers/';
@@ -86,7 +92,7 @@ export class Deployment extends Stack {
             }
         );
 
-        this.setupAppSync(this);
+        this.setupAppSync(this, users);
 
         const usersApiEndpoint = api.root.addResource('users');
 
@@ -448,19 +454,62 @@ export class Deployment extends Stack {
     //     }));
     // }
 
-    private setupAppSync(stack: Stack) {
-        const api = new GraphqlApi(this, `api-graphql-${settings.repositoryName}`, {
-            name: `api-graphql-${settings.repositoryName}`,
-            schema: Schema.fromAsset('cdk/schema.graphql'),
-            authorizationConfig: {
-                defaultAuthorization: {
-                    authorizationType: AuthorizationType.API_KEY,
-                    apiKeyConfig: {
-                        expires: Expiration.after(Duration.days(365)),
+    private setupAppSync(stack: Stack, users: Table) {
+        const api = new GraphqlApi(
+            this,
+            `graphql_api`,
+            {
+                name: `api-graphql-${settings.repositoryName}`,
+                schema: Schema.fromAsset('cdk/schema.graphql'),
+                authorizationConfig: {
+                    defaultAuthorization: {
+                        authorizationType: AuthorizationType.API_KEY,
+                        apiKeyConfig: {
+                            expires: Expiration.after(Duration.days(365)),
+                        },
                     },
                 },
-            },
-            xrayEnabled: true,
+                xrayEnabled: true,
+            }
+        );
+        const dynamodbDataSource = new DynamoDbDataSource(
+            this,
+            `api-graphql-${settings.repositoryName}-dynamo-data-source`,
+            {
+                api,
+                table: users,
+
+                description: 'DataSource for user table',
+                readOnlyAccess: true,
+            }
+        );
+
+        dynamodbDataSource.createResolver({
+            typeName: 'Query',
+            fieldName: 'getUserById',
+            requestMappingTemplate: MappingTemplate.fromString(`{
+                    "version": "2017-02-28",
+                    "operation": "GetItem",
+                    "key": {
+                      "id": $util.dynamodb.toDynamoDBJson($ctx.args.id)
+                    }
+                }`),
+            responseMappingTemplate: MappingTemplate.dynamoDbResultItem(),
+        });
+
+        dynamodbDataSource.createResolver({
+            typeName: 'Query',
+            fieldName: 'listUsers',
+            requestMappingTemplate: MappingTemplate.fromString(`{
+                "version": "2017-02-28",
+                "operation": "Scan",
+                "limit": $util.defaultIfNull($ctx.args.limit, 100),
+                "nextToken": $util.toJson($util.defaultIfNullOrBlank($ctx.args.nextToken, null))
+            }`),
+            responseMappingTemplate: MappingTemplate.fromString(`{
+                "items": $util.toJson($ctx.result.items),
+                "nextToken": $util.toJson($util.defaultIfNullOrBlank($context.result.nextToken, null))
+            }`),
         });
 
         // print out the AppSync GraphQL endpoint to the terminal
