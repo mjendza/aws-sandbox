76b0a4dc023f6302b136a2bf2b41bd5448fa9065
MJendza <mjendza@newforma.com>
2021-08-24 16:24:50 +0200

    small test refactor

--- test/cdk.unit.test.ts
+++ test/cdk.unit.test.ts
@@ -1,12 +1,6 @@
-import {
-    countResources,
-    expect,
-    haveResource,
-    haveResourceLike,
-} from '@aws-cdk/assert';
+import { expect, haveResource } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as sut from '../cdk/deployment';
-import { countResourcesLike } from '@aws-cdk/assert/lib/assertions/count-resources';
 
 test('CDK API Gateway REST APIs', () => {
     //GIVEN
@@ -24,43 +18,4 @@ test('CDK DynamoDB table', () => {
     const stack = new sut.Deployment(app, 'MyTestStack', {});
     // THEN
     expect(stack).to(haveResource('AWS::DynamoDB::Table'));
-});
-
-test('CDK Lambda functions', () => {
-    //GIVEN
-    const app = new cdk.App();
-    // WHEN
-    const stack = new sut.Deployment(app, 'MyTestStack', {});
-    //THEN
-    const result = haveResourceLike('AWS::Lambda::Function', {
-        Handler: 'index.handler',
-        Runtime: 'nodejs14.x',
-    });
-    expect(stack).to(result);
-    expect(stack).to(countResources('AWS::Lambda::Function', 9));
-});
-
-test('CDK EventBridge', () => {
-    //GIVEN
-    const app = new cdk.App();
-    // WHEN
-    const stack = new sut.Deployment(app, 'MyTestStack', {});
-    //THEN
-    expect(stack).to(haveResource('AWS::Events::EventBus'));
-    expect(stack).to(countResources('AWS::Events::EventBus', 1));
-});
-
-test('CDK EventBridge Rule - have only one to consume empty prefix (all events)', () => {
-    //GIVEN
-    const app = new cdk.App();
-    // WHEN
-    const stack = new sut.Deployment(app, 'MyTestStack', {});
-    //THEN
-    expect(stack).to(
-        countResourcesLike('AWS::Events::Rule', 1, {
-            EventPattern: {
-                source: [{ prefix: '' }],
-            },
-        })
-    );
 });
